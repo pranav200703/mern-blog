19e57041355821b281b80b2177b5be452335182d
pranav verma <pranavverma2007@gmail.com>
2024-04-09 14:22:31 +0530

    is admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -21,8 +21,12 @@ const userSchema = new mongoose.Schema({
         default:
           'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
+    isAdmin: {
+        type: Boolean,
+        default: false,
     },
-    {timestamps: true}/*adding for time of updation and creation */
+},
+{timestamps: true}/*adding for time of updation and creation */
 );
 /*creating model */
 const User = mongoose.model('User', userSchema);
