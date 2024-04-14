96e024c2f7d4b739491d5aee8eeca87be4258731
pranav verma <pranavverma2007@gmail.com>
2024-04-14 14:41:46 +0530

    add comment sction to the postpage with comment insertiong functionality

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.routes.js';
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -33,6 +34,7 @@ app.use('/api/user', userRoutes);
 /**signup routing */
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 /**middle ware for errors */
 app.use((err, req, res, next) => {
