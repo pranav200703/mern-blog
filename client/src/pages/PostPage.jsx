6ed3f036531a84fb8d3b859dba53e18c3898e86e
pranav verma <pranavverma2007@gmail.com>
2024-04-16 14:24:38 +0530

    home and post page creation

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -92,7 +92,7 @@ export default function PostPage() {
       {/*similar posts */}
       <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
-        <div className='flex flex-wrap md:flex-wrap lg:flex-nowrap gap-5 mt-4 justify-center'>
+        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
           {recentPosts &&
             recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
         </div>
